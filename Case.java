24b4087ca5bc3f195d0b4bddad49eaaba82751f8
bharathlokesh2007-dev <bharath.lokesh2007@gmail.com>
2026-02-01 16:03:06 +0530

    Add files via upload

---
+++ Case.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Case
+{
+public static void main(String[] args)
+{
+Scanner a = new Scanner(System.in);
+System.out.println("Enter a word: ");
+String str = a.nextLine();
+System.out.println(str.toUpperCase());
+System.out.println(str.toLowerCase());
+}
+}
