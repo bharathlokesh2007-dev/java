b9c5df25b7c671a7ffeb9c46e573020d2b004502
bharathlokesh2007-dev <bharath.lokesh2007@gmail.com>
2026-06-20 14:55:50 +0530

    Booking

---
+++ R_Pattern.java
@@ -0,0 +1,16 @@
+public class R_Pattern {
+    public static void main(String[] args) {
+        int n = 7;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (j == 1 ||(i == 1 && j < n) || (i == n/2+1 && j < n) ||  (j == n && i > 1 && i <= n/2) || (i - j == n/2))           
+                {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
